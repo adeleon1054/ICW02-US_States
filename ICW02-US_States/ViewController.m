f993da847480b326e4951b6f8962d4b5da38599f
Asdruval De Leon <adeleon1054@gmail.com>
2017-10-31 23:57:04 -0400

    alternate row background, bold and italic font and image display on the right - AD

--- ICW02-US_States/ViewController.m
+++ ICW02-US_States/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) NSArray *states, *statesCapital, *statesFlags;
 
 @end
 
@@ -17,6 +19,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    _states = @[@"Alabama", @"Alaska", @"Arizona", @"Arkansas", @"California", @"Colorado", @"Connecticut", @"Delaware",
+                @"Florida", @"Georgia", @"Hawaii", @"Idaho", @"Illinois", @"Indiana", @"Iowa", @"Kansas", @"Kentucky",
+                @"Louisiana", @"Maine", @"Maryland", @"Massachusetts", @"Michigan", @"Minnesota", @"Mississippi",
+                @"Missouri"];
+    
+    _statesCapital = @[@"Montgomery", @"Juneau", @"Phoenix", @"Little Rock", @"Sacramento", @"Denver", @"Hartford",
+                       @"Dover", @"Tallahassee", @"Atlanta", @"Honolulu", @"Boise", @"Springfield", @"Indianapolis",
+                       @"Des Moines", @"Topeka", @"Frankfort", @"Baton Rouge", @"Augusta", @"Annapolis", @"Boston",
+                       @"Lansing", @"St. Paul", @"Jackson", @"Jefferson City"];
+    
+    _statesFlags = @[@"al.png", @"ak.png", @"az.png", @"ar.png", @"ca.png", @"co.png", @"ct.png", @"de.png", @"fl.png",
+                     @"ga.png", @"hi.png", @"id.png", @"il.png", @"in.png", @"ia.png", @"ks.png", @"ky.png", @"la.png",
+                     @"me.png", @"md.png", @"ma.png", @"mi.png", @"mn.png", @"ms.png", @"mo.png"];
+
 }
 
 
@@ -25,5 +44,65 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [_states count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    //Define the type of tabel cell
+    static NSString* simpleTableIdentifier = @"SimpleTableIdentifier";
+    
+    //Ask the table to reuse previuosly used cell
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: simpleTableIdentifier];
+    
+    //if we need to re-create reusable cell, we test to see if nill (null)
+    if(cell == nil){
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    /* Display image on the left as default
+    UIImage *image = [UIImage imageNamed:[_statesFlags objectAtIndex:indexPath.row]];
+    cell.imageView.image = image; */
+    
+    cell.textLabel.text = _states[indexPath.row];
+    cell.detailTextLabel.text = _statesCapital[indexPath.row];
+    
+    //Display image on the right of the cell
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:[_statesFlags objectAtIndex:indexPath.row]]];
+    cell.accessoryView = imageView;
+    
+    cell.textLabel.font = [UIFont fontWithName:@"Verdana-BoldItalic" size:18];
+    
+    return cell;
+}
+
+#pragma mark UITableView Delegate Methods
+//Alternate row color
+- (void)tableView: (UITableView*)tableView willDisplayCell: (UITableViewCell*)cell forRowAtIndexPath: (NSIndexPath*)indexPath {
+    
+    if(indexPath.row % 2 == 0)
+        cell.backgroundColor = [UIColor lightGrayColor];
+    else
+        cell.backgroundColor = [UIColor whiteColor];
+}
+
+//Row selection
+-(NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if(indexPath.row == 0)
+        return nil;
+    else
+        return indexPath;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *rowState = _states[indexPath.row];
+    NSString *rowStateCapital = _statesCapital[indexPath.row];
+    NSString *message = [[NSString alloc]initWithFormat:@"You selected %@ and the capital is %@", rowState, rowStateCapital];
+    
+    NSLog(@"%@",message);
+    
+    //dismiss row selection
+    //[tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
 
 @end
